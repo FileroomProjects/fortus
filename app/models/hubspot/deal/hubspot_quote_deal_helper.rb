087bc635a9fa8c53a13163a04f675c38eb5c7257
brijesh994 <1515brijesh@gmail.com>
2025-11-20 17:27:43 +0530

    Optimized code

--- app/models/hubspot/deal/hubspot_quote_deal_helper.rb
+++ app/models/hubspot/deal/hubspot_quote_deal_helper.rb
@@ -2,18 +2,13 @@ module Hubspot::Deal::HubspotQuoteDealHelper
   extend ActiveSupport::Concern
 
   included do
-    def prepare_payload_for_netsuite_quote_deal(ns_quote_id)
-      {
-        "properties": {
-          "dealname": fetch_prop_field(:dealname),
-          "pipeline": ENV["HUBSPOT_DEFAULT_PIPELINE"], # Netsuite Quotes pipeline
-          "dealstage": ENV["HUBSPOT_DEFAULT_DEALSTAGE"], # Open stage
-          "netsuite_quote_id": ns_quote_id,
-          "amount": fetch_prop_field(:amount),
-          "netsuite_location": "https://#{ENV['NETSUITE_ACCOUNT_ID']}.suitetalk.api.netsuite.com/services/rest/record/v1/estimate/#{ns_quote_id}",
-          "netsuite_origin": "netsuite"
-        }
-      }
+    def create_and_update_hubspot_quote_deal(ns_quote)
+      netsuite_quote_deal_payload = prepare_payload_for_netsuite_quote_deal(ns_quote[:id])
+      hs_quote_deal = Hubspot::QuoteDeal.create(netsuite_quote_deal_payload)
+      if hs_quote_deal.present? && hs_quote_deal[:id].present?
+        Rails.logger.info "************** Created Hubspot Quote Deal with ID #{hs_quote_deal[:id]}"
+        association_for_deal(hs_quote_deal[:id])
+      end
     end
 
     def line_item_payload
@@ -25,5 +20,37 @@ module Hubspot::Deal::HubspotQuoteDealHelper
         }
       }
     end
+
+    private
+      def find_quote_deal
+        hs_deal = Hubspot::Deal.search(quote_deal_search_payload)
+        if hs_deal.present? && hs_deal[:id].present?
+          Rails.logger.info "************** Hubspot quote deal found ID #{hs_deal[:id]}"
+          hs_deal
+        end
+      end
+
+      def association_for_deal(hs_quote_deal_id)
+        Rails.logger.info "************** Associating Quote Deal with Company, Contact, Parent Deal and Line Item"
+        hs_quote_deal = Hubspot::QuoteDeal.new(hs_quote_deal_id, args[:objectId])
+        hs_quote_deal.associate_company
+        hs_quote_deal.associate_contact
+        hs_quote_deal.associate_parent_deal
+        hs_quote_deal.associate_line_item
+      end
+
+      def prepare_payload_for_netsuite_quote_deal(ns_quote_id)
+        {
+          "properties": {
+            "dealname": fetch_prop_field(:dealname),
+            "pipeline": ENV["HUBSPOT_DEFAULT_PIPELINE"], # Netsuite Quotes pipeline
+            "dealstage": ENV["HUBSPOT_DEFAULT_DEALSTAGE"], # Open stage
+            "netsuite_quote_id": ns_quote_id,
+            "amount": fetch_prop_field(:amount),
+            "netsuite_location": "https://#{ENV['NETSUITE_ACCOUNT_ID']}.suitetalk.api.netsuite.com/services/rest/record/v1/estimate/#{ns_quote_id}",
+            "netsuite_origin": "netsuite"
+          }
+        }
+      end
   end
 end
