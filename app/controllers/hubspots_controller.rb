2925bb9869cd76f2397a8aba2dcd5ae850221796
brijesh994 <1515brijesh@gmail.com>
2025-12-06 16:39:37 +0530

    Prevent child deal duplication and sync via estimates

--- app/controllers/hubspots_controller.rb
+++ app/controllers/hubspots_controller.rb
@@ -1,5 +1,6 @@
 class HubspotsController < ApplicationController
   protect_from_forgery with: :null_session
+  before_action :load_deal, only: [ :create_duplicate_ns_quote, :create_ns_quote ]
 
   def create_contact_customer
     @hubspot = Hubspot::Deal.new(params["hubspot"])
@@ -8,24 +9,15 @@ class HubspotsController < ApplicationController
   end
 
   def create_ns_quote
-    load_deal
-    process_estimate(
-      :prepare_payload_for_netsuite_estimate,
-      :create_hubspot_child_deal,
-      @hs_deal[:dealId]
-    )
+    payload = @hubspot.prepare_payload_for_netsuite_estimate
+    @ns_estimate = @hubspot.create_ns_estimate(payload)
 
     respond_to { |format| format.html }
   end
 
   def create_duplicate_ns_quote
-    load_deal
-    @parent_deal = @hubspot.find_parent_deal
-    process_estimate(
-      :prepare_payload_for_duplicate_netsuite_estimate,
-      :create_duplicate_hubspot_child_deal,
-      @parent_deal[:id]
-    )
+    payload = @hubspot.prepare_payload_for_duplicate_netsuite_estimate
+    @ns_estimate = @hubspot.create_ns_estimate(payload)
 
     respond_to { |format| format.html }
   end
@@ -34,13 +26,11 @@ class HubspotsController < ApplicationController
     def load_deal
       deal_id = params[:deal_id] || params[:dealId]
       @hs_deal = Hubspot::Deal.find_by(deal_id: deal_id)
-      @hubspot = Hubspot::Deal.new(@hs_deal)
-    end
 
-    def process_estimate(payload_method, create_deal_method, parent_deal_id)
-      payload = @hubspot.send(payload_method)
-      @ns_estimate = @hubspot.create_ns_estimate(payload)
-      @hs_child_deal = @hubspot.send(create_deal_method, @ns_estimate)
-      @hubspot.association_for_deal(@hs_child_deal[:id], parent_deal_id, @ns_estimate[:id])
+      unless @hs_deal.present?
+        render json: { error: "Deal not found" }, status: :not_found and return
+      end
+
+      @hubspot = Hubspot::Deal.new(@hs_deal)
     end
 end
